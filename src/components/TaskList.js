9dd960f21c9d01e4c849bbc92415fd4bcf14454c
rizve <rizve@Rizves-MacBook-Pro.local>
2023-12-27 15:52:21 +0100

    initial design done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
 export const TaskList = () => {
+
+    const tasks= [
+        {id: 101, name: "task 1", time: "2:09:01 AM 9/14/2030"},
+        {id: 102, name: "task 2", time: "2:09:01 AM 9/14/2030"},
+        {id: 103, name: "task 3", time: "2:09:01 AM 9/14/2030"},
+        {id: 104, name: "task 4", time: "2:09:01 AM 9/14/2030"},
+        {id: 105, name: "task 5", time: "2:09:01 AM 9/14/2030"}
+    ]
+
   return (
-    <div>TaskList</div>
+    <section className="showTask">
+        <div className="head">
+            <div>
+                <span className="title">Task</span>
+                <span className="count">0</span>
+            </div>
+            <button className='clearAll'>Clear All</button>
+        </div>
+        <ul>
+            {tasks.map((task)=> (
+                <li>
+                    <p>
+                        <span className="name">{task.name}</span>
+                        <span className="time">{task.time}</span>
+                    </p>
+                    <i className='bi bi-pencil-square'></i>
+                    <i className='bi bi-trash'></i>
+                </li>
+            ))}
+        </ul>
+        
+    </section>
   )
 }
